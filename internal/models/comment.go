9cfe537412d4d3778f5f982f56784e97b6e49748
Aleksandr <aleksandr@telexchange.ee>
2025-05-28 00:57:01 +0300

    style: formating code in models pkg

--- internal/models/comment.go
+++ internal/models/comment.go
@@ -6,16 +6,13 @@ import (
 )
 
 type Comment struct {
-	ID      uint   `gorm:"primaryKey" json:"id"`
-	Title   string `gorm:"size:255" json:"title"`
-	Content string `gorm:"type:text" json:"content"`
-
-	AuthorID uint `gorm:"index" json:"-"`                    // внешний ключ на User.ID
-	Author   User `gorm:"foreignKey:AuthorID" json:"author"` // Для получения автора через Preload
-
-	PostID uint `gorm:"index" json:"-"`                // внешний ключ на Post.ID
-	Post   Post `gorm:"foreignKey:PostID" json:"post"` // Для получения поста через Preload
-
+	ID        uint           `gorm:"primaryKey" json:"id"`
+	Title     string         `gorm:"size:255" json:"title"`
+	Content   string         `gorm:"type:text" json:"content"`
+	AuthorID  uint           `gorm:"index" json:"-"`                    // внешний ключ на User.ID
+	Author    User           `gorm:"foreignKey:AuthorID" json:"author"` // Для получения автора через Preload
+	PostID    uint           `gorm:"index" json:"-"`                    // внешний ключ на Post.ID
+	Post      Post           `gorm:"foreignKey:PostID" json:"post"`     // Для получения поста через Preload
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
--- internal/models/post.go
+++ internal/models/post.go
@@ -6,13 +6,11 @@ import (
 )
 
 type Post struct {
-	ID    uint   `gorm:"primarykey" json:"id"`
-	Title string `gorm:"size:255" json:"title"`
-	Text  string `gorm:"type:text" json:"text"`
-
-	AuthorID uint `gorm:"index" json:"-"`                    // внешний ключ на User.ID
-	Author   User `gorm:"foreignKey:AuthorID" json:"author"` // загружается через Preload
-
+	ID        uint           `gorm:"primarykey" json:"id"`
+	Title     string         `gorm:"size:255" json:"title"`
+	Text      string         `gorm:"type:text" json:"text"`
+	AuthorID  uint           `gorm:"index" json:"-"`                    // внешний ключ на User.ID
+	Author    User           `gorm:"foreignKey:AuthorID" json:"author"` // загружается через Preload
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`

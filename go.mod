d8a7ac29867f70291d8529660020a0cf48d55b51
Aleksandr <aleksandr@telexchange.ee>
2025-05-28 00:48:01 +0300

    feat: add configuration yaml + .env

--- go.mod
+++ go.mod
@@ -4,13 +4,15 @@ go 1.24.2
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.26.0 // indirect
 	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/gofiber/fiber/v2 v2.52.8 // indirect
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -26,7 +28,15 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sagikazarmark/locafero v0.7.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.12.0 // indirect
+	github.com/spf13/cast v1.7.1 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/spf13/viper v1.20.1 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
@@ -37,6 +47,7 @@ require (
 	golang.org/x/sync v0.14.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.25.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.5.11 // indirect
 	gorm.io/gorm v1.30.0 // indirect
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,86 @@
 package config
+
+import (
+	"github.com/go-viper/mapstructure/v2"
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+type Config struct {
+	Auth   AuthConfig   `mapstructure:"jwt"`
+	DB     DbConfig     `mapstructure:"database"`
+	Server ServerConfig `mapstructure:"server"`
+}
+
+type AuthConfig struct {
+	SigningKey string        `mapstructure:"signing_key"`
+	SecretKey  string        `mapstructure:"secret_key"`
+	AccessTTL  time.Duration `mapstructure:"access_ttl"`
+	RefreshTTL time.Duration `mapstructure:"refresh_ttl"`
+}
+
+type DbConfig struct {
+	Host     string `mapstructure:"host"`
+	Port     int    `mapstructure:"port"`
+	Database string `mapstructure:"database"`
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	SSLMode  string `mapstructure:"sslmode"`
+}
+
+type ServerConfig struct {
+	Port int    `mapstructure:"port"`
+	Mode string `mapstructure:"mode"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	// Подгрузка переменных окружения
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+		return nil, err
+	}
+
+	env := os.Getenv("APP_ENV")
+	if env == "" {
+		env = "dev"
+	}
+
+	// Указание имени и расширения файла
+	viper.SetConfigName(env)
+	viper.SetConfigType("yaml")
+
+	// Указание путей, где искать конфигурационный файл
+	viper.AddConfigPath(path)
+
+	// Загрузка переменных окружения
+	viper.SetEnvPrefix("APP")
+
+	// замена .на _в именах ключей, поэтому вложенные ключи, такие как, app.port могут напрямую сопоставляться с переменными среды
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+
+	var config *Config
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			log.Println("Config file not found, using environment variables only")
+		} else {
+			log.Fatalf("Ошибка чтения файла конфигурации, %s", err)
+		}
+	}
+	
+	err := viper.Unmarshal(&config, func(dc *mapstructure.DecoderConfig) {
+		dc.TagName = "mapstructure"
+		dc.Result = &config
+		dc.WeaklyTypedInput = true // позволяет преобразовывать строки в числа и т.п.
+	})
+	if err != nil {
+		log.Println("Ошибка при анмаршалинге в структуру")
+		return nil, err
+	}
+
+	return config, nil
+}

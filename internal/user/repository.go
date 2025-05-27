530cfe3ef8dc7d176642d1886edf7aaf2f5f54c3
Aleksandr <aleksandr@telexchange.ee>
2025-05-28 00:53:14 +0300

    feat: add user repo and init func logger

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,1 +1,18 @@
 package user
+
+import "gorm.io/gorm"
+
+type UserRepository interface {
+}
+
+type UserRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepositoryImpl {
+	return &UserRepositoryImpl{db: db}
+}
+
+func (repo *UserRepositoryImpl) FindByID(userId uint) error {
+	return repo.db.First(&userId).Error
+}

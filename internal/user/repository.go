9719d825acadbc77c8d8995b7288df2e88f80c27
Alexandr <alexandr@biznes-len.com>
2025-05-29 14:18:55 +0300

    feat: add user repo/service/handler

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -1,8 +1,16 @@
 package user
 
-import "gorm.io/gorm"
+import (
+	"github.com/crafty-ezhik/blog-api/internal/models"
+	"gorm.io/gorm"
+)
 
 type UserRepository interface {
+	FindByID(userId uint) (models.User, error)
+	FindByEmail(email string) (models.User, error)
+	Create(user *models.User) error
+	Update(user *models.User) error
+	Delete(userID uint) error
 }
 
 type UserRepositoryImpl struct {
@@ -13,6 +21,26 @@ func NewUserRepository(db *gorm.DB) *UserRepositoryImpl {
 	return &UserRepositoryImpl{db: db}
 }
 
-func (repo *UserRepositoryImpl) FindByID(userId uint) error {
-	return repo.db.First(&userId).Error
+func (service *UserRepositoryImpl) FindByID(userId uint) (models.User, error) {
+	var user models.User
+	result := service.db.First(&user, userId)
+	return user, result.Error
+}
+
+func (service *UserRepositoryImpl) FindByEmail(email string) (models.User, error) {
+	var user models.User
+	result := service.db.Where("email = ?", email).First(&user)
+	return user, result.Error
+}
+
+func (service *UserRepositoryImpl) Create(user *models.User) error {
+	return service.db.Create(user).Error
+}
+
+func (service *UserRepositoryImpl) Update(user *models.User) error {
+	return service.db.Save(user).Error
+}
+
+func (service *UserRepositoryImpl) Delete(userID uint) error {
+	return service.db.Delete(&userID).Error
 }
--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,39 @@
 package user
+
+import "github.com/crafty-ezhik/blog-api/internal/models"
+
+type UserService interface {
+	GetByID(userID uint) (models.User, error)
+	GetByEmail(email string) (models.User, error)
+	Create(user *models.User) error
+	Update(user *models.User) error
+	Delete(userID uint) error
+}
+
+type UserServiceImpl struct {
+	UserRepo UserRepository
+}
+
+func NewUserService(UserRepo UserRepository) *UserServiceImpl {
+	return &UserServiceImpl{UserRepo: UserRepo}
+}
+
+func (s *UserServiceImpl) GetByID(userID uint) (models.User, error) {
+	return s.UserRepo.FindByID(userID)
+}
+
+func (s *UserServiceImpl) GetByEmail(email string) (models.User, error) {
+	return s.UserRepo.FindByEmail(email)
+}
+
+func (s *UserServiceImpl) Create(user *models.User) error {
+	return s.UserRepo.Create(user)
+}
+
+func (s *UserServiceImpl) Update(user *models.User) error {
+	return s.UserRepo.Update(user)
+}
+
+func (s *UserServiceImpl) Delete(userID uint) error {
+	return s.UserRepo.Delete(userID)
+}

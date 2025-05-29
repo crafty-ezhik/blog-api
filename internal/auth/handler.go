db9f99734c2265b9f9550ac2dc6ff500dc51a8ae
Alexandr <alexandr@biznes-len.com>
2025-05-29 14:44:39 +0300

    refactor

--- internal/auth/handler.go
+++ internal/auth/handler.go
@@ -1,1 +1,21 @@
 package auth
+
+import (
+	"github.com/crafty-ezhik/blog-api/internal/user"
+	"github.com/gofiber/fiber/v2"
+)
+
+type AuthHandler interface {
+}
+
+type AuthHandlerImpl struct {
+	UserService user.UserService
+}
+
+func NewAuthHandler(userService user.UserService) *AuthHandlerImpl {
+	return &AuthHandlerImpl{UserService: userService}
+}
+
+func (h *AuthHandlerImpl) Login(c *fiber.Ctx) error {
+
+}
--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,18 +1,1 @@
 package user
-
-import "github.com/gofiber/fiber/v2"
-
-type AuthHandler interface {
-}
-
-type AuthHandlerImpl struct {
-	UserService UserService
-}
-
-func NewAuthHandler(userService UserService) *AuthHandlerImpl {
-	return &AuthHandlerImpl{UserService: userService}
-}
-
-func (h *AuthHandlerImpl) Login(c *fiber.Ctx) error {
-	
-}

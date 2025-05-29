caee80f9c137ede2a5cd965b8a496060f8d82c42
Alexandr <alexandr@biznes-len.com>
2025-05-29 13:00:11 +0300

    feat: add getting db connection and migrations

---
+++ db/connection.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"fmt"
+	"github.com/crafty-ezhik/blog-api/internal/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func GetConnection(conf *config.Config) *gorm.DB {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		conf.DB.Host,
+		conf.DB.Username,
+		conf.DB.Password,
+		conf.DB.Database,
+		conf.DB.Port,
+		conf.DB.SSLMode)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
--- migrations/auto.go
+++ migrations/auto.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/crafty-ezhik/blog-api/db"
+	"github.com/crafty-ezhik/blog-api/internal/config"
+	"github.com/crafty-ezhik/blog-api/internal/models"
+)
 
+func main() {
+	fmt.Println("Запуск автоматической миграции БД")
+	cfg, err := config.LoadConfig("./configs")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	DB := db.GetConnection(cfg)
+
+	err = DB.AutoMigrate(&models.User{}, &models.Post{}, &models.Comment{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Миграция успешно проведена!")
 }

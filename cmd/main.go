17ea9c459d767b11d631fc907d8963f456899ec9
Alexandr Kotov <sa.cotov@yandex.ru>
2025-05-31 01:29:37 +0300

    feat: init redis in app

--- cmd/main.go
+++ cmd/main.go
@@ -7,11 +7,21 @@ import (
 	"github.com/crafty-ezhik/blog-api/internal/config"
 	"github.com/crafty-ezhik/blog-api/internal/routes"
 	"github.com/crafty-ezhik/blog-api/internal/user"
+	"github.com/crafty-ezhik/blog-api/pkg/jwt"
 	"github.com/crafty-ezhik/blog-api/pkg/validate"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/redis/go-redis/v9"
 	"log"
 )
+
+/*
+1. Сделать генерацию токенов, обновление/отзыв/версионирование refresh токена
+2. Добавить логирование
+3. Подключить Swagger
+4. Поменять стандартный encoding/json на bytedance/sonic
+5. Добавить CORS middleware
+*/
 
 func main() {
 	// Init configuration
@@ -23,6 +33,17 @@ func main() {
 	// Get database connection
 	db := db2.GetConnection(cfg)
 
+	// Init redis
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", cfg.Redis.Host, cfg.Redis.Port),
+		Password: cfg.Redis.Password,
+		DB:       cfg.Redis.Db,
+	})
+
+	// Init JWT
+	jwtService := jwt.NewJWTService(jwt.NewRedisStorage(rdb))
+	jwtAuth := jwt.NewJWT(jwtService, cfg.Auth.AccessTTL, cfg.Auth.RefreshTTL, cfg.Auth.SigningKey)
+
 	// Init Validator
 	myValidator := validator.New()
 	v := &validate.XValidator{
@@ -31,6 +52,7 @@ func main() {
 
 	// Init Fiber App
 	app := fiber.New(fiber.Config{
+
 		// TODO: Добавить логирование через ZAP
 		// TODO: Подключить Swagger
 		// TODO: Поменять JSON Decoder/Encoder с encoding/json на bytedance/sonic
@@ -41,7 +63,7 @@ func main() {
 
 	// Services
 	userService := user.NewUserService(userRepo)
-	authService := auth.NewAuthService(cfg, userRepo)
+	authService := auth.NewAuthService(cfg, userRepo, jwtAuth)
 
 	// Handlers
 	authHandler := auth.NewAuthHandler(userService, authService, v)

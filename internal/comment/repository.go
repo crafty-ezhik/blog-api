0c10dd76342058f55e544b41d0a0b37a4f990069
Alexandr <alexandr@biznes-len.com>
2025-06-02 17:44:55 +0300

    feat: methods are defined for repositories

--- internal/comment/repository.go
+++ internal/comment/repository.go
@@ -1,1 +1,17 @@
 package comment
+
+import (
+	"github.com/crafty-ezhik/blog-api/internal/models"
+	"gorm.io/gorm"
+)
+
+type CommentRepository interface {
+	FindCommentsByPostID(postID uint) ([]models.Comment, error)
+	CreateCommentByPostID(comment models.Comment) error
+	UpdateCommentByCommentAndPostID(comment models.Comment) error
+	DeleteCommentByCommentAndPostID(comment models.Comment) error
+}
+
+type CommentRepositoryImpl struct {
+	db *gorm.DB
+}
--- internal/post/repository.go
+++ internal/post/repository.go
@@ -1,1 +1,18 @@
 package post
+
+import (
+	"github.com/crafty-ezhik/blog-api/internal/models"
+	"gorm.io/gorm"
+)
+
+type PostRepository interface {
+	FindALL() []models.Post
+	FindByID(postID uint) (models.Post, error)
+	Create(post models.Post) error
+	Update(post models.Post) error
+	Delete(post models.Post) error
+}
+
+type PostRepositoryImpl struct {
+	db *gorm.DB
+}

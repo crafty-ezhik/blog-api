8c3a77724970257771f0f109febfda90f545585b
Alexandr <alexandr@biznes-len.com>
2025-06-02 17:51:11 +0300

    feat: post repository methods are implemented

--- internal/post/repository.go
+++ internal/post/repository.go
@@ -6,13 +6,43 @@ import (
 )
 
 type PostRepository interface {
-	FindALL() []models.Post
-	FindByID(postID uint) (models.Post, error)
-	Create(post models.Post) error
-	Update(post models.Post) error
-	Delete(post models.Post) error
+	FindALL() ([]models.Post, error)
+	FindByID(postID uint) (*models.Post, error)
+	Create(post *models.Post) error
+	Update(postID uint, updatedFields *models.Post) error
+	Delete(postID uint) error
 }
 
 type PostRepositoryImpl struct {
 	db *gorm.DB
+}
+
+func NewPostRepository(db *gorm.DB) *PostRepositoryImpl {
+	return &PostRepositoryImpl{
+		db: db,
+	}
+}
+
+func (repo *PostRepositoryImpl) FindALL() ([]models.Post, error) {
+	var posts []models.Post
+	result := repo.db.Find(&posts)
+	return posts, result.Error
+}
+
+func (repo *PostRepositoryImpl) FindByID(postID uint) (*models.Post, error) {
+	var post models.Post
+	result := repo.db.First(&post, postID)
+	return &post, result.Error
+}
+
+func (repo *PostRepositoryImpl) Create(post *models.Post) error {
+	return repo.db.Create(&post).Error
+}
+
+func (repo *PostRepositoryImpl) Update(postID uint, updatedFields *models.Post) error {
+	return repo.db.Model(models.Post{ID: postID}).Updates(updatedFields).Error
+}
+
+func (repo *PostRepositoryImpl) Delete(postID uint) error {
+	return repo.db.Delete(&models.Post{ID: postID}).Error
 }

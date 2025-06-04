c933cc43c7132aa7977dc81438d9445eba86650a
Alexandr <alexandr@biznes-len.com>
2025-06-04 14:27:42 +0300

    test: add Registration test

--- internal/auth/service_test.go
+++ internal/auth/service_test.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"github.com/crafty-ezhik/blog-api/internal/config"
 	"github.com/crafty-ezhik/blog-api/internal/models"
 	mock_user "github.com/crafty-ezhik/blog-api/internal/user/mock"
@@ -79,13 +80,126 @@ func TestAuthServiceImpl_Login(t *testing.T) {
 			Password: password,
 		})
 
-		// 7.5 Производит проверку на наличие ошибкок и полей в ответе
+		// 7.5 Производит проверку на наличие ошибок и полей в ответе
 		assert.NoError(t, err)
 		assert.NotNil(t, resp.AccessToken)
 		assert.NotNil(t, resp.RefreshToken)
 	})
+
+	// 8. Случай, когда пользователь не найден
+	t.Run("User not found", func(t *testing.T) {
+		email := "notfound@example.com"
+		mockUserRepo.EXPECT().FindByEmail(email).Return(nil, errors.New(ErrInvalidCredentials))
+
+		resp, _, err := authService.Login(&LoginRequest{
+			Email:    email,
+			Password: "any",
+		})
+		assert.Error(t, err)
+		assert.EqualError(t, err, ErrInvalidCredentials)
+		assert.Nil(t, resp)
+	})
+
+	t.Run("Wrong password", func(t *testing.T) {
+		email := "test@example.com"
+		correctPassword := "test"
+		wrongPassword := "wrong"
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(correctPassword), bcrypt.DefaultCost)
+		require.NoError(t, err)
+
+		user := &models.User{
+			ID:       1,
+			Name:     "TestUser",
+			Email:    email,
+			Password: string(hashedPassword),
+		}
+
+		mockUserRepo.EXPECT().FindByEmail(email).Return(user, nil)
+
+		resp, _, err := authService.Login(&LoginRequest{
+			Email:    email,
+			Password: wrongPassword,
+		})
+		assert.Error(t, err)
+		assert.EqualError(t, err, ErrInvalidCredentials)
+		assert.Nil(t, resp)
+	})
 }
 
 func TestAuthServiceImpl_Register(t *testing.T) {
+	logger.Log, _ = zap.NewDevelopment()
+	defer logger.Log.Sync()
 
+	// 1. Объявляем контроллер и финишируем его
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// 2. Создаем мок userRepository, BlackList, TokenVersion передав туда контроллер
+	mockUserRepo := mock_user.NewMockUserRepository(ctrl)
+	mockBlackList := mock_jwt.NewMockBlackListStorage(ctrl)
+	mockTokenVersion := mock_jwt.NewMockTokenVersionStorage(ctrl)
+
+	// 3. Создаем экземпляр конфига
+	cfg := &config.Config{
+		Auth: config.AuthConfig{
+			SigningKey: "FKI/0XYt3YksmneW8QxCRWdlYbINIzPdp4fpiTqXXqs=",
+			SecretKey:  "",
+			AccessTTL:  time.Duration(30) * time.Minute,
+			RefreshTTL: time.Duration(30) * time.Hour,
+		},
+	}
+
+	// 4. Создаем экземпляр jwtService
+	jwtService := jwt.NewJWTService(mockBlackList, mockTokenVersion)
+	jwtAuth := jwt.NewJWT(jwtService, cfg.Auth.AccessTTL, cfg.Auth.RefreshTTL, cfg.Auth.SigningKey)
+
+	// 5. Создаем экземпляр AuthService
+	authService := &AuthServiceimpl{
+		cfg:      cfg,
+		jwtAuth:  jwtAuth,
+		UserRepo: mockUserRepo,
+	}
+
+	// 6. Начало тестов
+	t.Run("Successful register", func(t *testing.T) {
+		email := "test@example.com"
+		request := &RegisterRequest{
+			Name:     "Name",
+			Email:    email,
+			Password: "test",
+			Age:      30,
+		}
+
+		// Ожидаем, что пользователя нет в базе
+		mockUserRepo.EXPECT().FindByEmail(email).Return(nil, nil)
+
+		// Ожидаем вызов Create
+		mockUserRepo.EXPECT().Create(gomock.All()).DoAndReturn(func(user *models.User) error {
+			assert.Equal(t, request.Name, user.Name)
+			assert.Equal(t, request.Age, user.Age)
+			assert.Equal(t, request.Email, user.Email)
+			assert.NotEmpty(t, user.Password)
+			return nil
+		})
+
+		ok, err := authService.Register(request)
+		assert.NoError(t, err)
+		assert.True(t, ok)
+	})
+
+	t.Run("User already exists", func(t *testing.T) {
+		email := "test@example.com"
+		request := &RegisterRequest{
+			Email:    email,
+			Password: "test",
+		}
+
+		mockUserRepo.EXPECT().FindByEmail(email).Return(&models.User{Email: email}, nil)
+
+		ok, err := authService.Register(request)
+		assert.Error(t, err)
+		assert.False(t, ok)
+		assert.Contains(t, err.Error(), ErrUserExisted)
+	})
 }
